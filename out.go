b2e40d9b11f31cc0e831eb03e79665a321331f78
Marc Rene Arns <arns@stridor.net>
2018-10-23 18:57:00 +0200

    disable real closing of rtmidi out port, since it does not semm to work reliably

--- out.go
+++ out.go
@@ -3,7 +3,6 @@ package rtmididrv
 import (
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/gomidi/connect"
 	"github.com/gomidi/rtmididrv/imported/rtmidi"
@@ -80,13 +79,17 @@ func (o *out) Close() error {
 	o.closed = true
 	o.Unlock()
 
-	time.Sleep(time.Millisecond * 500)
-	o.Lock()
-	err := o.midiOut.Close()
-	o.Unlock()
-	if err != nil {
-		return fmt.Errorf("can't close MIDI out %v (%s): %v", o.number, o, err)
-	}
+	/*
+		time.Sleep(time.Millisecond * 500)
+		o.Lock()
+		err := o.midiOut.Close()
+		o.midiOut.Destroy()
+		o.Unlock()
+
+		if err != nil {
+			return fmt.Errorf("can't close MIDI out %v (%s): %v", o.number, o, err)
+		}
+	*/
 
 	return nil
 }
